34b4790c19c713c3baa71ebf975ee4a4fc9529f5
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-06-20 13:27:08 -0400

    Display Array in a table

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -16,19 +16,44 @@ class CoursesPage extends React.Component {
     // here you call then function, to handle the response.
     // promises are away to handle async call, you can also use async/await if you prefer.
     // promises represent future values. then function will recieve array of courses
-    getCourses().then(function (courses) {
-      // this.state.courses = courses; // do not do that, but use setState as follow
-      // note: setState calls only update the properties you specify
-      this.setState({ courses: courses });
-    });
+
+    //getCourses().then(function (courses) {
+    // this.state.courses = courses; // do not do that, but use setState as follow
+    // note: setState calls only update the properties you specify
+    // this.setState({ courses: courses });
+    //});
 
     // you can use arrow function like this
     // this line says: getCourses from the API. when the call completes, store the array of courses in state
-    // getCourses().then(courses => this.setState({courses: courses}));
+    getCourses().then((courses) => this.setState({ courses: courses }));
   }
 
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>AuthorId</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                <tr>
+                  <td>{course.title}</td>
+                  <td>{course.authorId}</td>
+                  <td>{course.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
